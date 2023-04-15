dcbfc283091e5eca5bd2f9ba3324fe7574abb3ba
panmourovaty <panmourovaty@centrum.cz>
2023-04-15 14:55:02 +0200

    Update pre-install.sh

--- installer/kickstart/pre-install.sh
+++ installer/kickstart/pre-install.sh
@@ -2,7 +2,7 @@
 
 set -oue pipefail
 
-DEFAULT_URL="registry.hub.docker.com/panmourovaty/uflinux2:latest"
+DEFAULT_URL="registry.hub.docker.com/panmourovaty/uflinux2:38"
 
 for ARG in `cat /proc/cmdline`; do
     if [[ "${ARG}" =~ ^imageurl= ]]; then
